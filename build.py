9cc17db4c5bdae739be541f3b6593b7781b0c0fc
Alex Kucher <a.kucher@avantize.com>
2017-12-13 01:43:37 +0200

    Updated README.md

--- build.py
+++ build.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     name = get_name_from_recipe()
     username, channel, version = get_env_vars()
     reference = "{0}/{1}".format(name, version)
-    upload = "https://api.bintray.com/bincrafters/public-conan"
+    upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
     builder = ConanMultiPackager(
         username=username,
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         upload=upload,
         remotes=upload,  # while redundant, this moves bincrafters remote to position 0
         upload_only_when_stable=True,
-        stable_branch_pattern="release/*")
+        stable_branch_pattern="stable/*")
 
     builder.add_common_builds(shared_option_name=name + ":shared")
     builder.run()
